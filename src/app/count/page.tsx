b4185f2eda1f7c457ac14822aac385059a89650d
Alexandru <alexdnc22@gmail.com>
2024-09-07 18:42:11 +0300

    v2 fix

--- src/app/count/page.tsx
+++ src/app/count/page.tsx
@@ -1,89 +1,13 @@
-'use client'
+import CountContent from '@/components/shared/CountContent';
+import { Suspense } from 'react';
 
-import { useRouter, useSearchParams } from 'next/navigation';
-import { Suspense, useCallback, useEffect, useState } from 'react';
-
-import { Button } from "@/components/ui/button";
-import CellGrid from '../../components/shared/CellGrid';
-import ResultsModal from '../../components/shared/ResultsModal';
 
 export default function Count() {
-  const searchParams = useSearchParams();
-  const maxCells = Number(searchParams.get('maxCells'));
-  const [cellCounts, setCellCounts] = useState<number[]>([]);
-  const [actionStack, setActionStack] = useState<number[]>([]);
-  const [showModal, setShowModal] = useState(false);
-
-  const router = useRouter(); 
-
-  useEffect(() => {
-    if (maxCells) {
-      setCellCounts(Array(12).fill(0));
-    }
-  }, [maxCells]);
-
-  const totalCount = cellCounts.reduce((a, b) => a + b, 0);
-
-  const handleCellClick = useCallback((index: number) => {
-    if (totalCount < maxCells) {
-      setCellCounts(prev => {
-        const newCounts = [...prev];
-        newCounts[index]++;
-        return newCounts;
-      });
-      setActionStack(prev => [...prev, index]);
-    }
-  }, [maxCells, totalCount]);
-
-  const handleUndo = useCallback(() => {
-    if (actionStack.length > 0) {
-      const lastAction = actionStack[actionStack.length - 1];
-      setCellCounts(prev => {
-        const newCounts = [...prev];
-        newCounts[lastAction]--;
-        return newCounts;
-      });
-      setActionStack(prev => prev.slice(0, -1));
-    }
-  }, [actionStack]);
-
-  const handleReset = () => {
-    setCellCounts(Array(12).fill(0));
-    setActionStack([]);
-  };
-
-  const handleFinish = () => {
-    setShowModal(true);
-  };
-
-  const handleBack = () => {
-    router.push('/');
-  };
-
   return (
-    <Suspense fallback={<div>Loading...</div>}>
     <div className="container mx-auto p-4">
-      <div className="flex-col justify-between items-center mb-4 xl:flex xl:flex-row">
-        <div className="mb-5">
-          <p>Maximum number of cells: <span className="font-bold">{maxCells}</span></p>
-          <p>Current count: <span className="font-bold">{totalCount}</span></p>
-        </div>
-        <div className="flex gap-2 justify-center xl:justify-end">
-          <Button onClick={handleBack}>Back</Button>
-          <Button onClick={handleUndo} disabled={actionStack.length === 0}>Undo</Button>
-          <Button onClick={handleReset}>Reset count</Button>
-          <Button onClick={handleFinish} disabled={totalCount !== maxCells}>Finish</Button>
-        </div>
-      </div>
-      <CellGrid cellCounts={cellCounts} onCellClick={handleCellClick} />
-      <ResultsModal
-        maxCells={maxCells}
-        cellCounts={cellCounts}
-        onClose={() => setShowModal(false)}
-        isOpen={showModal}
-        onOpenChange={setShowModal}
-      />
+      <Suspense fallback={<div>Loading...</div>}>
+        <CountContent />
+      </Suspense>
     </div>
-    </Suspense>
   );
 }
